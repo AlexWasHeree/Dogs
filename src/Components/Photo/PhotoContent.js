b1a041c553291eff6a8937ace348310b5da90011
AlexWasHeree <victoralexandre2303@gmail.com>
2022-09-12 00:23:23 -0300

    comments

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -11,17 +11,19 @@ const PhotoContent = ({ data }) => {
         <img src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
-        <p>
-          <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
-          <span className={styles.visualizacoes}>{photo.acessos}</span>
-        </p>
-        <h1 className="title">
-          <Link to={`/photo/${photo.id}`}>{photo.title}</Link>
-        </h1>
-        <ul className={styles.atributos}>
-          <li>{photo.peso} kg</li>
-          <li>{photo.idade} anos</li>
-        </ul>
+        <div>
+          <p className={styles.author}>
+            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            <span className={styles.visualizacoes}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} kg</li>
+            <li>{photo.idade} anos</li>
+          </ul>
+        </div>
       </div>
       <PhotoComments id={photo.id} comments={comments} />
     </div>
